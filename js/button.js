2d5ea34df79c46779931267a0b809b519f5bba55
owlstep <stephielhong@gmail.com>
2018-07-31 21:24:05 -0700

    added button functionality to homepage

---
+++ js/button.js
@@ -0,0 +1,17 @@
+aboutButton = $('.main .features .button-feature.button-about');
+
+aboutButton.on('click', function() {
+    window.location = 'about.html';
+});
+
+gameProjectsButton = $('.main .features .button-feature.button-game');
+
+gameProjectsButton.on('click', function() {
+    window.location = 'projects.html';
+});
+
+mediaButton = $('.main .features .button-feature.button-media');
+
+mediaButton.on('click', function() {
+    window.location = 'media.html';
+});
