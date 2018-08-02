eb47544bb497668b54b008f844e0bb73785dc992
owlstep <stephielhong@gmail.com>
2018-08-01 19:07:29 -0700

    need to fix: pokemon.js cant find ship

--- js-projects/pokemon.js
+++ js-projects/pokemon.js
@@ -1,21 +1,76 @@
 
 sendBtn = $('button.submit-btn');
+var pokemonName = $('p.pokemon-name');
+var pokemonAbility = $('p.pokemon-ability');
+var pokemonChosen;
 
 sendBtn.on('click', function () {
     var x = $('input.pokemon-input').val();
     if (x != undefined) { 
         $.ajax({
-            type: 'GET',
-            url: "http://pokeapi.co/api/v2/pokemon/" + x + "/",
+            url: "https://swapi.co/api/people/" + x + "/",
             contentType: 'json',
 
             success: function(result) {
-                $('p.pokemon-name').text() = result.name;
+                pokemonChosen = result;
+                pokemonName.text(result.name);
+
+                if (result.films === undefined || result.films.length === 0) {
+                    pokemonAbility.text("Does not own a ship");
+                } else {
+                    var arrayOfShips = [];
+                    $.when(
+                        $.each(result.films, function(film) {
+                            $.ajax({
+                                url: film,
+                                contentType: 'json',
+    
+                                success: function(value) {
+                                    arrayOfShips.push(value.title);
+                                    console.log(value.title);
+                                },
+                                error: function() {
+                                    console.log("cant find my ship");
+                                }
+                            });
+                        })
+                    ).then( function() {
+                    console.log(arrayOfShips);
+                    console.log(arrayOfShips[1]);
+                    pokemonAbility.text(arrayOfShips.join(', '));
+                    });
+                }
             },
+
             error: function() {
-                $('p.pokemon-name').text = "Not discovered yet";
+                pokemonName.text("Not discovered yet");
+                pokemonAbility.text("Does not have TM-fly");
             }
 
         });
     }
 });
+
+battleBtn = $('.battle-start');
+battleText = $('.battle-text');
+
+battleBtn.on('click', function(){
+    randomPokemonGenerator();
+});
+
+var randomPokemonGenerator = function () {
+    var randNum = Math.floor(Math.random()*88);
+    $.ajax({
+        type: 'GET',
+        url: "https://swapi.co/api/people/" + randNum + "/",
+        contentType: 'json',
+        success: function(char) {
+            battleText.text("Wild " + char.name.toUpperCase() + " appeared!");
+        },
+        error: function() {
+            console.log("Could not find opponent");
+        } 
+    });
+}
+
+//https://rickandmortyapi.com/api/character/2
