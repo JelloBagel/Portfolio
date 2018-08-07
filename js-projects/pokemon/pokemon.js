e56c3853a28175435547d6ba2a85a8cf3e82b483
owlstep <stephielhong@gmail.com>
2018-08-07 01:44:18 -0700

    added styles and images, limited moves to 10

--- js-projects/pokemon/pokemon.js
+++ js-projects/pokemon/pokemon.js
@@ -1,9 +1,11 @@
+//http://pokeapi.salestock.net/api/v2/pokemon/ for WAMP
+//https://pokeapi.co/api/v2/pokemon/ for web
 var inputPokemonName = $('input.input');
 var enterBtn = $('button.button');
 var randBtn = $('button.rand-btn');
 var loadSpinner = $('div.loader');
 var pokemonName = $('p.output');
-var pokemonAbility = $('p.output-2');
+var pokemonMoves = $('form.output-2');
 var pokemonChosen;
 
 var battleBtn = $('button.battle-btn');
@@ -16,31 +18,51 @@ function loadingUpdate(loader) {
 }
 
 function getPokemon(pokemonChosen) {
+    if (pokemonChosen) {
     loadingUpdate(loadSpinner);
-    var urlInput = "https://pokeapi.co/api/v2/pokemon/" + pokemonChosen;
-    $.ajax({
-        url: urlInput,
-        dataType: 'json',
-        type: 'GET',
-        
-        success: function(pokemon) {
-            loadSpinner.hide();
-            pokemonName.text(pokemon.name.toUpperCase());
-            getAbilities(pokemon);
-        },
+        var urlInput = "https://pokeapi.co/api/v2/pokemon/" + pokemonChosen;
+        $.ajax({
+            url: urlInput,
+            dataType: 'json',
+            type: 'GET',
+            
+            success: function(pokemon) {
+                loadSpinner.hide();
+                pokemonName.text(pokemon.name.toUpperCase());
+                getMoves(pokemon);
+                getImage(pokemon);
+            },
 
-        error: function() {
-            loadSpinner.hide();
-            pokemonName.text("Not in the Pokedex");
+            error: function() {
+                loadSpinner.hide();
+                pokemonName.text("Not in the Pokedex");
+            }
+        });
+    }
+}
+
+function getMoves(pokemon) {
+    var movesArray = pokemon.moves.map(function(moveObj) {
+        return moveObj.move.name;
+    }); 
+
+    //too many moves for player to choose from -> limit selection 10 randomized
+    var randomNumArray = [];
+    while (randomNumArray.length < 10) {
+        var randNum = Math.floor(Math.random()*pokemon.moves.length + 1);
+        if (!randomNumArray.includes(randNum)) {//if the randomnum is not already in the numarray add to num array 
+            randomNumArray.push(randNum);
         }
+    };
+
+    //creates new checkbox per move found so player can select 4 moves for battle
+    $.each(randomNumArray, function(num) {
+        pokemonMoves.append(`<input type="checkbox" name="pokemonMove" value="${movesArray[num]}">${movesArray[num]} <br>`)
     });
 }
 
-function getAbilities(pokemon) {
-    var abilityArray = pokemon.abilities.map(function(abilityObj) {
-        return abilityObj.ability.name;
-    });
-    pokemonAbility.text(abilityArray.join(", "));
+function getImage(pokemon) {
+    $('div.pokemon-img').prepend(`<img class="poke-img" src="${pokemon.sprites.front_default}" />`);
 }
 
 function randOpponent() {
@@ -83,6 +105,7 @@ function battle(opponent) {
     loadSpinner2.hide();
 }
 
+
 enterBtn.on('click', function() {
     getPokemon(inputPokemonName.val());
 });
@@ -92,7 +115,18 @@ randBtn.on('click', function() {
 });
 
 battleBtn.on('click', function(){
-    randOpponent();
+    randOpponent(); //not clickable until player selects a pokemon
+    //display battlefield only when battle start is clicked 
+    $(".battlefield").prepend('<img src="./grass-battlefield.png" alt="grass battlefield">');
 });
+
+//clear previous pokemon chose
+//print image of pokemon and opponent
+//get pokemon ability for damage
+//select 4 moves to use to battle
+//double dmg for weakness
+//animate pokemon attack
+//replace input bar with search and select
+
 
 
