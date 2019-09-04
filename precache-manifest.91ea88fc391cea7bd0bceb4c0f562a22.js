04c454013402934861045b09a89b6357b76810bc
owlstep <stephielhong@gmail.com>
2019-09-03 22:31:11 -0700

    Updates

---
+++ precache-manifest.91ea88fc391cea7bd0bceb4c0f562a22.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1699fcb40e45027df9e15f313fc5e9be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "287e25eff8cbb055177c",
+    "url": "/static/css/main.69b8dcba.chunk.css"
+  },
+  {
+    "revision": "3418ea40c984b0b0027f",
+    "url": "/static/js/2.937d2398.chunk.js"
+  },
+  {
+    "revision": "287e25eff8cbb055177c",
+    "url": "/static/js/main.0ca6cbad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ba8534bbfa441f073c9ee6a9d3d41a32",
+    "url": "/static/media/highlight-bg-chars.ba8534bb.png"
+  },
+  {
+    "revision": "d628d14ca7fd14652a000bbd329a2327",
+    "url": "/static/media/overcooked.d628d14c.png"
+  },
+  {
+    "revision": "ff44b1b0287c3b0d631093b92f38a898",
+    "url": "/static/media/pachimaru.ff44b1b0.png"
+  },
+  {
+    "revision": "9aa57fbadd0d0f5d6ee57b9a68d893bb",
+    "url": "/static/media/puppy-rumble.9aa57fba.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-website/precache-manifest.f10c99f17203c4b11039f4d7f2558620.js"
+  "/precache-manifest.91ea88fc391cea7bd0bceb4c0f562a22.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
